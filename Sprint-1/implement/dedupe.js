86a348ea5e2ec36b9474813aff65aaf0c2bdf306
ldfajardo10-tech <ld.fajardo10@gmail.com>
2025-11-26 14:42:07 +0100

    dedupe exercise solution

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,8 @@
-function dedupe() {}
+function dedupe(arr) {
+    return [...new Set (arr)];
+}
+console.log(dedupe([1, 2, 3]));
+console.log(dedupe([1, 1, 2, 3, 2, 3, 3]));
+console.log(dedupe([]));
+console.log(dedupe(["hello", undefined, 5, 5, "hello"]));
+
