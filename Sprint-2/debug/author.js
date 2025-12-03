1d223bc078feb7e46e52f137150ed35c91c260cd
ldfajardo10-tech <ld.fajardo10@gmail.com>
2025-12-03 18:43:08 +0100

    exercise author solution

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-
+// we should take the object and access to the values through dotation.
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +11,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
