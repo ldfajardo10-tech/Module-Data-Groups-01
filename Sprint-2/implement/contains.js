2a84cab268bc2b7a6544486b4488114f58352d33
ldfajardo10-tech <ld.fajardo10@gmail.com>
2025-12-03 20:06:05 +0100

    comments update exercise contains

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,12 +1,12 @@
 function contains(obj, prop) {
 
-    if (typeof obj !== "object" && obj === null ) {
+    if (typeof obj !== "object" && obj === null ) { //typeof to check if the key is an object
         return false;
-    } else if (Array.isArray(obj)) {
+    } else if (Array.isArray(obj)) { //array.isarray to check that the obj is an array, so we throw an error message
         throw new Error ("invalid input"); 
     }
 
-    return prop in obj; 
+    return prop in obj; //in method to check if a property is in the object
 
 }
 console.log(contains({ a: 1, b: 2 }, "a"));
@@ -15,7 +15,7 @@ console.log(contains({}));
 console.log(contains({undefined},"b"));
 
 try {
-    console.log(contains([1, 2, 3], "3"));
+    console.log(contains([1, 2, 3], "3")); 
 }  catch (E) {
     console.log(E.message);
 }
