be54ebf8468fc6e6ff11f9b9cef518c6ca83070b
ldfajardo10-tech <ld.fajardo10@gmail.com>
2025-12-03 19:59:45 +0100

    exercise constains solution

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,23 @@
-function contains() {}
+function contains(obj, prop) {
+
+    if (typeof obj !== "object" && obj !== null ) {
+        return false;
+    } else if (Array.isArray(obj)) {
+        throw new Error ("invalid input"); 
+    }
+
+    return prop in obj; 
+
+}
+console.log(contains({ a: 1, b: 2 }, "a"));
+console.log(contains({ a: 1, b: 2 }, "c"));
+console.log(contains({}));
+console.log(contains({undefined},"b"));
+
+try {
+    console.log(contains([1, 2, 3], "3"));
+}  catch (E) {
+    console.log(E.message);
+}
 
 module.exports = contains;
