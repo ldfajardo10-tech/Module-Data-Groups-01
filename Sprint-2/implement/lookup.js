397fba6c9d916da68cea90fecdec4e3d080c2f2e
ldfajardo10-tech <ld.fajardo10@gmail.com>
2025-12-05 19:24:03 +0100

    exercise lookup solution

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,19 @@
-function createLookup() {
-  // implementation here
-}
+function createLookup(codePairs) {
+
+  if (!Array.isArray(codePairs)) {
+    return null;
+  }
+
+  return Object.fromEntries(codePairs); //convert the code pairs arrays into object
+
+} 
+
+    const countryCurrency = [
+     ["US", "USD"],
+     ["CA", "CAD"],
+   ];
+
+const result = createLookup(countryCurrency);
+console.log(result);
 
 module.exports = createLookup;
