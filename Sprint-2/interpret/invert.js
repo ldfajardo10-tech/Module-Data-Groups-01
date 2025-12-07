15c78958bca09ec5d5ff9234b0ad0d4f9268949b
ldfajardo10-tech <ld.fajardo10@gmail.com>
2025-12-07 15:36:15 +0100

    invert exercise solution

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -10,20 +10,34 @@ function invert(obj) {
   const invertedObj = {};
 
   for (const [key, value] of Object.entries(obj)) {
-    invertedObj.key = value;
+    invertedObj[value] = key;
   }
 
   return invertedObj;
 }
 
-// a) What is the current return value when invert is called with { a : 1 }
+// a) What is the current return value when invert is called with { a : 1 } = { key : 1}
 
-// b) What is the current return value when invert is called with { a: 1, b: 2 }
+// b) What is the current return value when invert is called with { a: 1, b: 2 } = { key : 2}
 
-// c) What is the target return value when invert is called with {a : 1, b: 2}
+// c) What is the target return value when invert is called with {a : 1, b: 2} = { 1 : a, 2 : b}
 
-// c) What does Object.entries return? Why is it needed in this program?
+// c) What does Object.entries return? Why is it needed in this program? = returns an array of key, value pairs. It's important bc we can loop both, key and values.
 
-// d) Explain why the current return value is different from the target output
+// d) Explain why the current return value is different from the target output = because invertedObj.key is just setting a property called "key" 
+// we need to use the [] to access to the variable.
 
-// e) Fix the implementation of invert (and write tests to prove it's fixed!)
+// e) Fix the implementation of invert (and write tests to prove it's fixed!) 
+
+test("inverts object with numeric strings value", () => {
+  expect(invert({ a: 1, b: 2 })).toBe({ 1: a, 2: b });
+});
+
+test("inverts object with strings value", () => {
+  expect(invert({ a: "hello", b: "world" })).toBe({ "hello": a, "world": b });
+});
+
+test("inverts object with numeric strings value and literal strings value", () => {
+  expect(invert({ a: "hello", b: 5 })).toBe({ hello: a, 5 : b });
+});
+
