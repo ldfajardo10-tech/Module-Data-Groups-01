6b982ad2358226f894703cd8a5f931be56de2728
ldfajardo10-tech <ld.fajardo10@gmail.com>
2025-12-07 12:44:20 +0100

    tally exercise solution

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,27 @@
-function tally() {}
+function tally(arr) {
+  
+    if (!Array.isArray(arr)) {
+      throw new TypeError("Input must be an array");
+    }  
+    
+    const counts = {}; //store the result as an object 
+
+      for (const items of arr) { //for... of loop to count the items 
+        if (counts[items]) {
+          counts[items] += 1;
+        } else {
+          counts[items] = 1;
+        }
+      }  
+
+    return counts;
+
+}
+
+console.log(tally(["a","a","c"]));
+console.log(tally(["a", "5", "5"]));
+console.log(tally([]));
+console.log(tally("hello world"));
+
 
 module.exports = tally;
