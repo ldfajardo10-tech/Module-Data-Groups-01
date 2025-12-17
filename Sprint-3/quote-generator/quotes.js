17dbcf335d04f3321e8f96fed2a5ec5fbabe2ad4
ldfajardo10-tech <ld.fajardo10@gmail.com>
2025-12-17 15:12:17 +0100

    quote generator app

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -491,3 +491,16 @@ const quotes = [
 ];
 
 // call pickFromArray with the quotes array to check you get a random quote
+const quoteElement = document.getElementById("quote");
+const authorElement = document.getElementById("author");
+const button = document.getElementById("new-quote");
+
+function pickRandomQuote () {
+  const randomQuote = pickFromArray(quotes);
+  quoteElement.textContent = `"${randomQuote.quote}"`;
+  authorElement.textContent = `-${randomQuote.author}`;
+}
+
+pickRandomQuote();
+
+button.addEventListener("click", pickRandomQuote);
