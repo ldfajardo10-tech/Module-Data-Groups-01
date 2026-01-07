42993dd671b67186f35e8f94fb5e5ceacaf21d51
ldfajardo10-tech <ld.fajardo10@gmail.com>
2026-01-07 17:50:27 +0100

    refactoring implementation and fixing the test syntax

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,12 +1,18 @@
 function findMax(elements) {
-    const nums = elements.filter(i => typeof i === "number" && !isNaN(i));
+    const arr = elements.filter(i => typeof i === "number" && !isNaN(i));
     
-    if (nums.length === 0) {
-        return "-infinity";
-    } else {
-        return Math.max(...nums);
+    if (arr.length === 0) {
+        return -Infinity;
     }
-  
-}
 
+    let max = arr[0];
+
+    for (const num of arr) {
+        if (num > max) {
+            max = num;
+        }
+    }
+    
+    return max;
+}
 module.exports = findMax;
